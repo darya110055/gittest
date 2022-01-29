366c167d76b6b70048c99fe5a93eb8ec7cb46517
darya110055 <darya110055@gmail.com>
2022-01-29 13:04:44 +0500

    task 6 filing

--- Task6/Task6_Filing.sql
+++ Task6/Task6_Filing.sql
@@ -1,9 +1,8 @@
-INSERT INTO `buy` (`CodeRoute`, `PurposeTravel`, `Price`, `Number`, `Date`) VALUES
-	(2, 3, 54201.00, 434, '2022-01-29'),
-	(4, 4, 54201.00, 434, '2022-01-29'),
-	(1, 3, 54201.00, 434, '2022-01-29'),
-	(4, 1, 54201.00, 434, '2022-01-29'),
-	(5, 1, 54201.00, 434, '2022-01-29');
+INSERT INTO `target` (`Code`, `PurposeRout`) VALUES
+	(2, 'Экскурсия'),
+	(3, 'Лечение'),
+	(4, 'Обучение'),
+	(1, 'Отдых');
 INSERT INTO `country` (`CodeCountry`, `NameCountry`, `PriceVisa`) VALUES
 	(2, 'США', 24.00),
 	(3, 'Россия', 0.00),
@@ -13,8 +12,9 @@ INSERT INTO `route` (`CodeRoute`, `CodeCountry`, `NameRoute`) VALUES
 	(4, 2, 'кп'),
 	(5, 1, 'кепукп'),
 	(2, 3, 'мвапавм');
-INSERT INTO `target` (`Code`, `PurposeRout`) VALUES
-	(2, 'Экскурсия'),
-	(3, 'Лечение'),
-	(4, 'Обучение'),
-	(1, 'Отдых');
+INSERT INTO `buy` (`CodeRoute`, `PurposeTravel`, `Price`, `Number`, `Date`) VALUES
+	(2, 3, 54201.00, 434, '2022-01-29'),
+	(4, 4, 54201.00, 434, '2022-01-29'),
+	(1, 3, 54201.00, 434, '2022-01-29'),
+	(4, 1, 54201.00, 434, '2022-01-29'),
+	(5, 1, 54201.00, 434, '2022-01-29');
