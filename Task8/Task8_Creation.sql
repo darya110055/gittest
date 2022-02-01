8038dfae1aff412d199fbb49b395652f6af3b871
darya110055 <darya110055@gmail.com>
2022-02-01 11:29:34 +0500

    task 8

--- Task8/Task8_Creation.sql
+++ Task8/Task8_Creation.sql
@@ -1,25 +1,31 @@
+-- Дамп структуры базы данных publications
 CREATE DATABASE IF NOT EXISTS `publications` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `publications`;
 
+-- Дамп структуры для таблица publications.publications
 CREATE TABLE IF NOT EXISTS `publications` (
-  `Index` int(11) NOT NULL,
-  `PublicationType` text DEFAULT NULL,
-  `NamePublication` text DEFAULT NULL,
-  `Price` decimal(10,0) DEFAULT NULL,
-  KEY `Index` (`Index`)
+  `Index` int(11) NOT NULL AUTO_INCREMENT,
+  `PublicationType` text NOT NULL,
+  `NamePublication` text NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`Index`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Дамп структуры для таблица publications.recipient
 CREATE TABLE IF NOT EXISTS `recipient` (
-  `CodeRecipient` int(11) NOT NULL,
-  `FullName` text DEFAULT NULL,
-  `Address` text DEFAULT NULL,
-  KEY `CodeRecipient` (`CodeRecipient`)
+  `CodeRecipient` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` text NOT NULL,
+  `Address` text NOT NULL,
+  PRIMARY KEY (`CodeRecipient`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Дамп структуры для таблица publications.subscription
 CREATE TABLE IF NOT EXISTS `subscription` (
   `CodePecipient` int(11) NOT NULL,
   `IndexPublication` int(11) NOT NULL,
-  `Period` int(11) DEFAULT NULL,
-  `Month` int(11) DEFAULT NULL,
-  `Year` year(4) DEFAULT NULL,
+  `Period` int(11) NOT NULL,
+  `Month` int(11) NOT NULL,
+  `Year` year(4) NOT NULL,
   KEY `FK_subscription_recipient` (`CodePecipient`),
   KEY `FK_subscription_publications` (`IndexPublication`),
   CONSTRAINT `FK_subscription_publications` FOREIGN KEY (`IndexPublication`) REFERENCES `publications` (`Index`),
