fd849dad0339531dede3b656b6eeda432f46ad9b
darya110055 <darya110055@gmail.com>
2022-02-01 11:24:10 +0500

    task 7

--- Task7/Task7_Creation.sql
+++ Task7/Task7_Creation.sql
@@ -1,22 +1,23 @@
+-- Дамп структуры базы данных trade
 CREATE DATABASE IF NOT EXISTS `trade` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `trade`;
 
 -- Дамп структуры для таблица trade.department
 CREATE TABLE IF NOT EXISTS `department` (
-  `CodeDepartment` int(11) NOT NULL,
-  `NameDepartment` text DEFAULT NULL,
-  `FullNameManager` text DEFAULT NULL,
+  `CodeDepartment` int(11) NOT NULL AUTO_INCREMENT,
+  `NameDepartment` text NOT NULL,
+  `FullNameManager` text NOT NULL,
   `Phone` int(11) DEFAULT NULL,
-  `SalesPerDay` decimal(10,2) DEFAULT NULL,
-  KEY `CodeDepartment` (`CodeDepartment`)
+  `SalesPerDay` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`CodeDepartment`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- Дамп структуры для таблица trade.product
 CREATE TABLE IF NOT EXISTS `product` (
-  `Article` int(11) NOT NULL,
-  `NameProduct` text DEFAULT NULL,
-  `Unit` text DEFAULT NULL,
-  `Price` decimal(10,2) DEFAULT NULL,
-  KEY `Article` (`Article`)
+  `ArticleProduct` int(11) NOT NULL AUTO_INCREMENT,
+  `NameProduct` text NOT NULL,
+  `Unit` text NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`ArticleProduct`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- Дамп структуры для таблица trade.sale
 CREATE TABLE IF NOT EXISTS `sale` (
@@ -27,5 +28,5 @@ CREATE TABLE IF NOT EXISTS `sale` (
   KEY `FK_sale_product` (`ArticleProduct`),
   KEY `FK_sale_department` (`Departament`),
   CONSTRAINT `FK_sale_department` FOREIGN KEY (`Departament`) REFERENCES `department` (`CodeDepartment`),
-  CONSTRAINT `FK_sale_product` FOREIGN KEY (`ArticleProduct`) REFERENCES `product` (`Article`)
+  CONSTRAINT `FK_sale_product` FOREIGN KEY (`ArticleProduct`) REFERENCES `product` (`ArticleProduct`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
