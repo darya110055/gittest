b15858e7ff3fb2e2d5ab51c1263160eac0797160
darya110055 <darya110055@gmail.com>
2022-02-01 09:28:04 +0500

    task1

--- Task1/Task1_Creation.sql
+++ Task1/Task1_Creation.sql
@@ -10,6 +10,15 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `Phone` int(11) DEFAULT NULL,
   KEY `Id` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Дамп структуры для таблица supply.product
+CREATE TABLE IF NOT EXISTS `product` (
+  `CodeProduct` int(11) NOT NULL,
+  `NameProduct` text DEFAULT NULL,
+  `PriceProduct` decimal(10,2) DEFAULT NULL,
+  KEY `CodeProduct` (`CodeProduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Дамп структуры для таблица supply.order
 CREATE TABLE IF NOT EXISTS `order` (
   `CodeOrder` int(11) NOT NULL,
@@ -24,13 +33,7 @@ CREATE TABLE IF NOT EXISTS `order` (
   CONSTRAINT `FK_order_customer` FOREIGN KEY (`IdCustomer`) REFERENCES `customer` (`Id`),
   CONSTRAINT `FK_order_product` FOREIGN KEY (`CodeProduct`) REFERENCES `product` (`CodeProduct`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- Дамп структуры для таблица supply.product
-CREATE TABLE IF NOT EXISTS `product` (
-  `CodeProduct` int(11) NOT NULL,
-  `NameProduct` text DEFAULT NULL,
-  `PriceProduct` decimal(10,2) DEFAULT NULL,
-  KEY `CodeProduct` (`CodeProduct`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Дамп структуры для таблица supply.shipment
 CREATE TABLE IF NOT EXISTS `shipment` (
   `CodeShipment` int(11) NOT NULL,
