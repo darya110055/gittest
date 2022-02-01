7b6471dba620a77b438760341274bb64ff8f9713
darya110055 <darya110055@gmail.com>
2022-02-01 10:49:26 +0500

    task 4

--- Task4/Task4_Creation.sql
+++ Task4/Task4_Creation.sql
@@ -4,27 +4,32 @@ USE ` audit`;
 
 -- Дамп структуры для таблица  audit.category
 CREATE TABLE IF NOT EXISTS `category` (
-  `CodeCategory` int(11) NOT NULL,
+  `CodeCategory` int(11) NOT NULL AUTO_INCREMENT,
   `Category` text DEFAULT NULL,
   `RateFor1Hour` int(11) DEFAULT NULL,
-  KEY `Код категории` (`CodeCategory`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`CodeCategory`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
 -- Дамп структуры для таблица  audit.staff
 CREATE TABLE IF NOT EXISTS `staff` (
-  `CodeEmployee` int(11) NOT NULL,
-  `FullName` text DEFAULT NULL,
-  `NPasport` int(11) DEFAULT NULL,
-  `DateOfBirth` date DEFAULT NULL,
-  `Phone` int(11) DEFAULT NULL,
-  KEY `Код сотрудника` (`CodeEmployee`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `CodeEmployee` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` text NOT NULL,
+  `NPasport` int(11) NOT NULL,
+  `DateOfBirth` date NOT NULL,
+  `Phone` decimal(10,0) DEFAULT NULL,
+  `Category` int(11) NOT NULL,
+  PRIMARY KEY (`CodeEmployee`),
+  KEY `FK_staff_category` (`Category`),
+  CONSTRAINT `FK_staff_category` FOREIGN KEY (`Category`) REFERENCES `category` (`CodeCategory`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
 -- Дамп структуры для таблица  audit.work
 CREATE TABLE IF NOT EXISTS `work` (
   `CompanyName` text NOT NULL,
   `CodeEmployee` int(11) NOT NULL,
-  `Category` int(11) NOT NULL,
   `Date` date DEFAULT NULL,
   `NumberOfHoursWorked` int(11) DEFAULT NULL,
   KEY `FK_work_staff` (`CodeEmployee`),
-  KEY `FK_work_category` (`Category`)
+  CONSTRAINT `FK_work_staff` FOREIGN KEY (`CodeEmployee`) REFERENCES `staff` (`CodeEmployee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
