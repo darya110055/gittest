92b64ffd5ad03c62a76aea80de86212122229f6b
darya110055 <98509691+darya110055@users.noreply.github.com>
2022-02-03 11:06:47 +0500

    task 11

--- Task11/Task11_Creation.sql
+++ Task11/Task11_Creation.sql
@@ -1,33 +1,36 @@
--- Дамп структуры базы данных overalls
-CREATE DATABASE IF NOT EXISTS `overalls` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `overalls`;
 USE `overalls`;
--- Дамп структуры для таблица overalls.overalls
 CREATE TABLE IF NOT EXISTS `overalls` (
   `CodeOveralls` int(11) NOT NULL AUTO_INCREMENT,
   `ViewOveralls` text NOT NULL,
-  `WearPeriod` int(11) NOT NULL,
-  `Price` decimal(10,2) DEFAULT NULL,
+  `WearPeriodInMonths` int(11) NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`CodeOveralls`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- Дамп структуры для таблица overalls.workshops
+CREATE TABLE IF NOT EXISTS `position` (
+  `CodePosition` int(11) NOT NULL,
+  `NamePosition` text NOT NULL,
+  `Sale, %` decimal(2,0) NOT NULL,
+  PRIMARY KEY (`CodePosition`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE IF NOT EXISTS `workshops` (
   `CodeWorkshops` int(11) NOT NULL AUTO_INCREMENT,
   `NameWorkshops` text NOT NULL,
   `FullNameChiefWorkshops` text NOT NULL,
   PRIMARY KEY (`CodeWorkshops`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- Дамп структуры для таблица overalls.workers
 CREATE TABLE IF NOT EXISTS `workers` (
   `CodeWorkers` int(11) NOT NULL AUTO_INCREMENT,
   `Workshop` int(11) NOT NULL,
   `FullName` text NOT NULL,
-  `Post` text NOT NULL,
-  `Sale, %` decimal(2,0) DEFAULT NULL,
+  `Post` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`CodeWorkers`),
   KEY `FK_workers_workshops` (`Workshop`),
+  KEY `FK_workers_position` (`Post`),
+  CONSTRAINT `FK_workers_position` FOREIGN KEY (`Post`) REFERENCES `position` (`CodePosition`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_workers_workshops` FOREIGN KEY (`Workshop`) REFERENCES `workshops` (`CodeWorkshops`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- Дамп структуры для таблица overalls.receiving
 CREATE TABLE IF NOT EXISTS `receiving` (
   `CodeWorkers` int(11) NOT NULL,
   `CodeOveralls` int(11) NOT NULL,
@@ -37,6 +40,4 @@ CREATE TABLE IF NOT EXISTS `receiving` (
   KEY `FK_receiving_overalls` (`CodeOveralls`),
   CONSTRAINT `FK_receiving_overalls` FOREIGN KEY (`CodeOveralls`) REFERENCES `overalls` (`CodeOveralls`),
   CONSTRAINT `FK_receiving_workers` FOREIGN KEY (`CodeWorkers`) REFERENCES `workers` (`CodeWorkers`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
--- Task11/Task11_Filing.sql
+++ Task11/Task11_Filing.sql
@@ -1,4 +1,4 @@
-INSERT INTO `overalls` (`CodeOveralls`, `ViewOveralls`, `WearPeriod`, `Price`) VALUES
+INSERT INTO `overalls` (`CodeOveralls`, `ViewOveralls`, `WearPeriodInMonths`, `Price`) VALUES
 	(1, 'A', 12, 56.41),
 	(2, 'B', 77, 56.41),
 	(3, 'C', 5, 56.41),
@@ -9,12 +9,18 @@ INSERT INTO `workshops` (`CodeWorkshops`, `NameWorkshops`, `FullNameChiefWorksho
 	(3, 'BHrtth', 'Галкин Ярослав Артемьевич'),
 	(4, 'BHnfg', 'Богомолова Марьям Никитична'),
 	(5, 'Vgftfmk', 'Мухина Анна Алексеевна');
-INSERT INTO `workers` (`CodeWorkers`, `Workshop`, `FullName`, `Post`, `Sale, %`) VALUES
-	(1, 3, 'Константинова Елизавета Андреевна', 'djhd', NULL),
-	(2, 3, 'Кудрявцев Фёдор Кириллович', 'djhd', 50),
-	(3, 4, 'Михайлова Мария Михайловна', 'djhd', 30),
-	(4, 1, 'Кузнецова Ксения Саввична', 'djhd', 45),
-	(5, 2, 'Соколов Артемий Александрович', 'djhd', NULL);
+INSERT INTO `position` (`CodePosition`, `NamePosition`, `Sale,%`) VALUES
+	(1, 'FGnbdfn', 30),
+	(2, 'BGndnm', 30),
+	(3, 'BHrtth', 40),
+	(4, 'BHnfg', 35),
+	(5, 'Vgftfmk', 50);	
+INSERT INTO `workers` (`CodeWorkers`, `Workshop`, `FullName`, `Post`) VALUES
+	(1, 3, 'Константинова Елизавета Андреевна', 1),
+	(2, 3, 'Кудрявцев Фёдор Кириллович', 2),
+	(3, 4, 'Михайлова Мария Михайловна', 3),
+	(4, 1, 'Кузнецова Ксения Саввична', 5),
+	(5, 2, 'Соколов Артемий Александрович', 4);
 INSERT INTO `receiving` (`CodeWorkers`, `CodeOveralls`, `Date`, `Painting`) VALUES
 	(2, 3, '2022-02-01', 'fdhg'),
 	(4, 1, '2022-02-01', 'dhdg'),
