6118d52f1ff7c123150b33138471e1182ae671ff
darya110055 <98509691+darya110055@users.noreply.github.com>
2022-02-08 11:35:47 +0500

    task 14

--- Task14/Task14_Creation.sql
+++ Task14/Task14_Creation.sql
@@ -6,6 +6,18 @@ CREATE TABLE IF NOT EXISTS `view_dish` (
   `NameViewDish` text NOT NULL,
   PRIMARY KEY (`CodeViewDish`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `dish` (
+  `CodeDish` int(11) NOT NULL AUTO_INCREMENT,
+  `NameDish` text NOT NULL,
+  `ViewDish` int(11) NOT NULL,
+  `ServingWeight,g` decimal(3,0) NOT NULL DEFAULT 0,
+  `Pucture` blob DEFAULT NULL,
+  `CookingTime` int(11) NOT NULL,
+  `Recipe` text NOT NULL,
+  PRIMARY KEY (`CodeDish`),
+  KEY `FK_dish_view_dish` (`ViewDish`),
+  CONSTRAINT `FK_dish_view_dish` FOREIGN KEY (`ViewDish`) REFERENCES `view_dish` (`CodeViewDish`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 CREATE TABLE IF NOT EXISTS `cooking` (
   `CodeDish` int(11) NOT NULL AUTO_INCREMENT,
   `NumberPortions` int(11) NOT NULL,
@@ -20,18 +32,6 @@ CREATE TABLE IF NOT EXISTS `product` (
   `ProductWeight` decimal(10,2) NOT NULL DEFAULT 0.00,
   `Price` decimal(10,2) NOT NULL DEFAULT 0.00,
   PRIMARY KEY (`CodeProduct`)
-CREATE TABLE IF NOT EXISTS `dish` (
-  `CodeDish` int(11) NOT NULL AUTO_INCREMENT,
-  `NameDish` text NOT NULL,
-  `ViewDish` int(11) NOT NULL,
-  `ServingWeight,g` decimal(3,0) NOT NULL DEFAULT 0,
-  `Pucture` blob DEFAULT NULL,
-  `CookingTime` int(11) NOT NULL,
-  `Recipe` text NOT NULL,
-  PRIMARY KEY (`CodeDish`),
-  KEY `FK_dish_view_dish` (`ViewDish`),
-  CONSTRAINT `FK_dish_view_dish` FOREIGN KEY (`ViewDish`) REFERENCES `view_dish` (`CodeViewDish`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 CREATE TABLE IF NOT EXISTS `dish_product` (
   `Dish` int(11) NOT NULL,
   `Product` int(11) NOT NULL,
